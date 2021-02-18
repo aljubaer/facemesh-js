91df0219bc00d7668e0627baaef43c11c092f12e
aljubaer <bsse0812@iit.du.ac.bd>
2021-02-18 14:06:03 +0600

    setup the dependencies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
+import * as tf from '@tensorflow/tfjs';
+import * as facemesh from '@tensorflow-models/facemesh';
+import Webcam from 'react-webcam';
+
 function App() {
   return (
     <div className="App">
